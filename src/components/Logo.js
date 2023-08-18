bf725637ee646b40ec4bab8a920e9c49b31d9cfa
mamadou <mimadou97@yahoo.com>
2023-08-18 15:14:07 +0200

    application pour pays

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+
+      <h3>The world's countries</h3>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/pages/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <div  className="a_propos">
+        <h1>A propos</h1>
+      <br />
+      <p>
+      Je m’appelle Mamadou DIOP et je suis étudiant en master 2 à l’institut Européen de Formation en ingénierie Informatique: F2I . J’ai créé ce site pour vous facilter la recherche d'information sur les différentes pays du monde.
+
+      Ce site permet d'avoir des informations sur l'ensemble des pays du monde. Grâce à ce site vous pouvez connaitre la capitale, le nombre d'habitant et le drapeau de votre pays 
+      </p>
+      <br />
+      <p>
+        
+      </p>
+      </div>
+      
+    </div>
+  );
+};
+
+export default About;
